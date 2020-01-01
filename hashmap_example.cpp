2f6f269800c4e836a92c8060498e1bcca98168dc
AKYOL <ugur-akyoll@hotmail.com>
2020-01-01 20:25:35 +0300

    Last Edition

--- hashmap_example.cpp
+++ hashmap_example.cpp
@@ -2,11 +2,13 @@
 #include <cstdlib> 
 #include<cstring>
 #include<string>
+#include<string.h>
 #include <time.h>
 #include<stdlib.h>
 #include <fstream>
 #include<map>
 #include<iterator>
+#include <sstream> 
 //#include <bits/stdc++.h> 
 
 using namespace std;
@@ -42,110 +44,25 @@ struct product {
     }
 };
 
-
-
-void read_csv_file(map<int, product> data);
+void read_csv_file(map<int, product> *data);
+void search(map<int, product> *data );
+void add_to_stock(map<int, product> *data,int row);
 
 int main()
 {
     
     map<int, product> data;
+    map<int, product> data2;
      
     data.insert({0,product("invoice_no","stock_code","description","quantity","invoice_date","unit_price","costomer_id","country") });
+   
+    read_csv_file(&data);
 
-    string str = "storage.csv";
+    search(&data);
 
-    ifstream file(str);
+   // add_to_stock(&data,1);
+ 
 
-	if (file.is_open()) {
-
-		string line;
-        int row = 0;
-		while (getline(file, line)) {
-			
-			if(row == 0){
-
-            }else{
-                string invoice_no = "";
-                string stock_code = "";
-                string description = "";
-                string quantity = "";
-                string invoice_date = "";
-                string unit_price = "";
-                string costomer_id = "";
-                string country = "";
-                
-                string s = line.c_str();
-                std::string delimiter = ",";
-                size_t pos = 0;
-                std::string token;
-                int coloum = 0;
-
-                while ((pos = s.find(delimiter)) != std::string::npos) {
-                    coloum ++;
-                                
-                    switch (coloum)
-                    {
-                    case 1:
-                    invoice_no = token;
-                    break;
-                    case 2:
-                    stock_code = token;
-                    break;
-                    case 3:
-                    description = token;
-                    break;
-                    case 4:
-                    quantity = token;
-                    break;
-                    case 5:
-                    invoice_date = token;
-                    break;
-                    case 6:
-                    unit_price = token;
-                    break;
-                    case 7:
-                    costomer_id = token;
-                    break;
-                    case 8:
-                    country = token;
-                    break;
-                    
-                    default:
-                    break;
-                    }
-                    token = s.substr(0, pos);
-                    s.erase(0, pos + delimiter.length());
-                }
-                /*
-                cout << row<<" " << invoice_no <<" "<<stock_code <<" "<<description <<
-                " "<< quantity <<" "<< invoice_no <<" "<< unit_price <<" "<< costomer_id <<
-                " "<< country << " \n";
-                */
-                data.insert({row,product(invoice_no,stock_code,description,quantity,invoice_date,unit_price,costomer_id,country) });
-                   
-            }
-			row++;
-		}
-		file.close();
-	}      
-
-    map<int, product>::iterator it;
-
-    for(it=data.begin();it!=data.end();it++)
-     cout << it->first <<" " << it->second.invoice_no <<" "<<it->second.stock_code <<" "<<it->second.description <<
-        " "<<it->second.quantity <<" "<< it->second.invoice_no <<" "<< it->second.unit_price <<" "<<it->second.costomer_id <<
-        " "<< it->second.country << " \n";
-    int id;
-    cout << "Please, enter row id : ";
-    cin>>id;
-    it = data.find(id);
-    if (it != data.end())
-    {
-        cout << it->first <<" " << it->second.invoice_no <<" "<<it->second.stock_code <<" "<<it->second.description <<
-        " "<<it->second.quantity <<" "<< it->second.invoice_no <<" "<< it->second.unit_price <<" "<<it->second.costomer_id <<
-        " "<< it->second.country << " \n";
-    }
 
 
 
@@ -169,7 +86,7 @@ int main()
 */
 }
 
-void read_csv_file(map<int, product> data){
+void read_csv_file(map<int, product> *data){
 
   string str = "storage.csv";
   ifstream file(str);
@@ -236,16 +153,74 @@ void read_csv_file(map<int, product> data){
                     token = s.substr(0, pos);
                     s.erase(0, pos + delimiter.length());
                 }
-                cout << row<<" " << invoice_no <<" "<<stock_code <<" "<<description <<
-                " "<< quantity <<" "<< invoice_no <<" "<< unit_price <<" "<< costomer_id <<
-                " "<< country << " \n";
-
-                data.insert({row,product(invoice_no,stock_code,description,quantity,invoice_date,unit_price,costomer_id,country) });
-                   
+                
+                data->insert({row,product(invoice_no,stock_code,description,quantity,invoice_date,unit_price,costomer_id,country) });
+                  
             }
 			row++;
 		}
 		file.close();
-	}      
-   // return data;
+	}     
 }
+
+void add_to_stock(map<int, product> *data,int row){
+
+    map<int, product>::iterator it;
+
+    for(it=data->begin();it!=data->end();it++)
+
+     cout << it->first <<" " << it->second.invoice_no <<" "<<it->second.stock_code <<" "<<it->second.description <<
+        " "<<it->second.quantity <<" "<< it->second.invoice_no <<" "<< it->second.unit_price <<" "<<it->second.costomer_id <<
+        " "<< it->second.country << " \n";
+
+
+    int id;
+    cout << "Please, enter row id : ";
+    cin>>id;
+
+    it = data->find(id);
+    if (it != data->end())
+    {
+        cout << it->first <<" " << it->second.invoice_no <<" "<<it->second.stock_code <<" "<<it->second.description <<
+        " "<<it->second.quantity <<" "<< it->second.invoice_no <<" "<< it->second.unit_price <<" "<<it->second.costomer_id <<
+        " "<< it->second.country << " \n";
+    }
+}
+
+void search(map<int, product> *data ){
+
+    int f_stock_code;
+    cout << "Please, enter stock code : ";
+    cin>>f_stock_code;
+
+    cout<<endl<<f_stock_code;
+    map<int, product>::iterator it;
+
+ 
+    for(it=data->begin();it!=data->end();it++){               
+      
+        int x = 0; 
+        std::string s = it->second.stock_code;  
+        stringstream geek(s); 
+        
+        geek >> x;
+
+        if(x == f_stock_code){
+                cout<<"equal"<<endl;
+        }
+      
+        cout<< it->second.stock_code <<endl;
+
+        if(x == f_stock_code){
+            cout<<f_stock_code<<" in if  "<<  it->first <<" " << it->second.invoice_no <<" "<<it->second.stock_code <<" "<<it->second.description <<
+        " "<<it->second.quantity <<" "<< it->second.invoice_no <<" "<< it->second.unit_price <<" "<<it->second.costomer_id <<
+        " "<< it->second.country << " \n";
+
+        }
+
+    }
+        
+     
+
+}
+
